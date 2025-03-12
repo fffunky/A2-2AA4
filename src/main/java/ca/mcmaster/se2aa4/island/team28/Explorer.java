cad8d1bb1fc275530f7920ea345112066f0bd398
Keon Madani <keonmadani@frida.local>
2025-03-12 14:29:47 -0400

    make drone turn and scan a few times [closes #1, closes #2]

--- src/main/java/ca/mcmaster/se2aa4/island/team28/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team28/Explorer.java
@@ -12,21 +12,50 @@ public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
 
+    public boolean scanOrFly = false;
+    public boolean leftOrRight = false;
+    public String direction;
+    public Integer batteryLevel;
+
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
-        String direction = info.getString("heading");
-        Integer batteryLevel = info.getInt("budget");
+        direction = info.getString("heading");
+        batteryLevel = info.getInt("budget");
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
+
     }
 
     @Override
     public String takeDecision() {
         JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
+
+        if (batteryLevel > 6900) {
+            if (scanOrFly) {
+                decision.put("action", "heading");
+                JSONObject parameters = new JSONObject();
+                if (leftOrRight) {
+                    parameters.put("direction", "E");
+                    leftOrRight = false;
+                } else {
+                    parameters.put("direction", "S");
+                    leftOrRight = true;
+                }
+                decision.put("parameters", parameters);
+                scanOrFly = false;
+            } else{
+                decision.put("action", "scan");
+                scanOrFly = true;
+            }
+        } else {
+            decision.put("action", "stop");
+        }
+
+
+
         logger.info("** Decision: {}",decision.toString());
         return decision.toString();
     }
@@ -34,9 +63,14 @@ public class Explorer implements IExplorerRaid {
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        //JSONObject extraInfo = response.getJSONObject("extras");
+
+        //Response res = new Response(response.getInt("cost"), response.getString("status"), extraInfo.);
+
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
+        batteryLevel -= cost;
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
